4e1680def7989667e15a5ca14dbb8320d46469f4
Wenzel Reichmuth <wenzel.reichmuth@vraktion.com>
2019-08-13 15:47:49 +0200

    modified defines for being unique
    Signed-off-by: Wenzel Reichmuth <wenzel.reichmuth@vraktion.com>

--- LIS2DH12.cpp
+++ LIS2DH12.cpp
@@ -25,7 +25,7 @@ LIS2DH12::LIS2DH12(SPI *_spi, DigitalOut *_cs) : i2c(NULL),
 }
 
 uint8_t LIS2DH12::whoIAm(){
-    char regAddr = (char)WHO_AM_I;
+    char regAddr = (char)LIS2DH12_WHO_AM_I;
     char regData;
     readFromReg(regAddr, (uint8_t*)&regData, 1);
     return (uint8_t)regData;
@@ -36,8 +36,8 @@ uint8_t LIS2DH12::setMode(Mode mode){
     char ctrl4Copy;
     uint8_t success;
 
-    readFromReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
-    readFromReg((char)CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
+    readFromReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    readFromReg((char)LIS2DH12_CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
 
     switch(mode){
         case HIGH_RES:
@@ -55,24 +55,24 @@ uint8_t LIS2DH12::setMode(Mode mode){
         default:
             return 0;
     }
-    writeToReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
-    success = writeToReg((char)CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
+    writeToReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    success = writeToReg((char)LIS2DH12_CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
     return success;
 }
 
 uint8_t LIS2DH12::enableAxes(Axis axis){
     char ctrl1Copy;
-    readFromReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    readFromReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
     ctrl1Copy |= axis;
-    writeToReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    writeToReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
     return 0;
 }
 
 uint8_t LIS2DH12::disableAxes(Axis axis){
     char ctrl1Copy;
-    readFromReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    readFromReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
     ctrl1Copy &= ~(1 << axis);
-    writeToReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    writeToReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
     return 0;
 }
 
@@ -90,8 +90,8 @@ int16_t LIS2DH12::readXAxis(){
 
     //readFromReg((char)OUT_X_H, (uint8_t*)&tempData, 1);
     //rawData = (int8_t)tempData << 8;
-    readFromReg((char)OUT_X_L, (uint8_t*)&rawData, 1);
-    readFromReg((char)OUT_X_H, ((uint8_t*)&rawData)+1, 1);
+    readFromReg((char)LIS2DH12_OUT_X_L, (uint8_t*)&rawData, 1);
+    readFromReg((char)LIS2DH12_OUT_X_H, ((uint8_t*)&rawData)+1, 1);
 
     if (rawData >= 0)
         rawData = (rawData>>4);
@@ -106,8 +106,8 @@ int16_t LIS2DH12::readYAxis(){
     char tempData;
     //readFromReg((char)OUT_Y_H, (uint8_t*)&tempData, 1);
     //rawData = (int8_t)tempData << 8;
-    readFromReg((char)OUT_Y_L, (uint8_t*)&rawData, 1);
-    readFromReg((char)OUT_Y_H, ((uint8_t*)&rawData)+1, 1);
+    readFromReg((char)LIS2DH12_OUT_Y_L, (uint8_t*)&rawData, 1);
+    readFromReg((char)LIS2DH12_OUT_Y_H, ((uint8_t*)&rawData)+1, 1);
 
     if (rawData >= 0)
         rawData = (rawData>>4);
@@ -122,8 +122,8 @@ int16_t LIS2DH12::readZAxis(){
     char tempData;
     //readFromReg((char)OUT_Z_H, (uint8_t*)&tempData, 1);
     //rawData = (int8_t)tempData << 8;
-    readFromReg((char)OUT_Z_L, (uint8_t*)&rawData, 1);
-    readFromReg((char)OUT_Z_H, ((uint8_t*)&rawData)+1, 1);
+    readFromReg((char)LIS2DH12_OUT_Z_L, (uint8_t*)&rawData, 1);
+    readFromReg((char)LIS2DH12_OUT_Z_H, ((uint8_t*)&rawData)+1, 1);
 
     if (rawData >= 0)
         rawData = (rawData>>4);
@@ -135,17 +135,17 @@ int16_t LIS2DH12::readZAxis(){
 
 uint8_t LIS2DH12::setODR(Odr odr){
     char ctrl1Copy;
-    readFromReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    readFromReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
     ctrl1Copy |= (odr << 4);
-    writeToReg((char)CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
+    writeToReg((char)LIS2DH12_CTRL_REG1, (uint8_t*)&ctrl1Copy, 1);
     return 0;
 }
 
 uint8_t LIS2DH12::setScale(Scale scale){
     char ctrl4Copy;
-    readFromReg((char)CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
+    readFromReg((char)LIS2DH12_CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
     ctrl4Copy |= (scale << 4);
-    writeToReg((char)CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
+    writeToReg((char)LIS2DH12_CTRL_REG4, (uint8_t*)&ctrl4Copy, 1);
     return 0;
 }
 
@@ -154,39 +154,39 @@ uint8_t LIS2DH12::setScale(Scale scale){
 // parametara: THS, DUR, Latch...
 uint8_t LIS2DH12::int1Setup(uint8_t setup){
     char data = setup;
-    writeToReg((char)CTRL_REG3, (uint8_t*)&data, 1);
+    writeToReg((char)LIS2DH12_CTRL_REG3, (uint8_t*)&data, 1);
     return 0;
 }
 
 uint8_t LIS2DH12::int1Latch(uint8_t enable){
     char ctrl5Copy;
-    readFromReg((char)CTRL_REG5, (uint8_t*)&ctrl5Copy, 1);
+    readFromReg((char)LIS2DH12_CTRL_REG5, (uint8_t*)&ctrl5Copy, 1);
     ctrl5Copy |= enable;
-    writeToReg((char)CTRL_REG5, (uint8_t*)&ctrl5Copy, 1);
+    writeToReg((char)LIS2DH12_CTRL_REG5, (uint8_t*)&ctrl5Copy, 1);
     return 0;
 }
 
 uint8_t LIS2DH12::int1Duration(uint8_t duration){
     char copy = duration;
-    writeToReg((char)INT1_DURATION, (uint8_t*)&copy, 1);
+    writeToReg((char)LIS2DH12_INT1_DURATION, (uint8_t*)&copy, 1);
     return 0;
 }
 
 uint8_t LIS2DH12::int1Threshold(uint8_t threshold){
     char copy = threshold;
-    writeToReg((char)INT1_THS, (uint8_t*)&copy, 1);
+    writeToReg((char)LIS2DH12_INT1_THS, (uint8_t*)&copy, 1);
     return 0;
 }
 
 uint8_t LIS2DH12::int1Config(uint8_t config){
     char copy = config;
-    writeToReg((char)INT1_CFG, (uint8_t*)&copy, 1);
+    writeToReg((char)LIS2DH12_INT1_CFG, (uint8_t*)&copy, 1);
     return 0;
 }
 
 void LIS2DH12::clearIntFlag(){
     char data;
-    readFromReg((char)INT1_SRC, (uint8_t*)&data, 1);
+    readFromReg((char)LIS2DH12_INT1_SRC, (uint8_t*)&data, 1);
 }
 
 uint8_t LIS2DH12::readFromReg(uint8_t regAddr, uint8_t *buff, size_t buffSize)
